9f901b4e9d4d6b50ef85254dd36a657f52875fce
funfish22 <j26391442@gmail.com>
2020-01-14 17:36:21 +0800

    first commit

---
+++ precache-manifest.ece16abcc3cb30c23f9c21811eb1c14c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba83c40016d06eba2255c4cf5017e259",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ec7d249b26ae19b71475",
+    "url": "./static/css/main.7f76ee57.chunk.css"
+  },
+  {
+    "revision": "9b937d5d3f169c272d20",
+    "url": "./static/js/2.36710c6e.chunk.js"
+  },
+  {
+    "revision": "fd228f9b9a68ce6b23a6cf8b0a38a0e3",
+    "url": "./static/js/2.36710c6e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ec7d249b26ae19b71475",
+    "url": "./static/js/main.7bceb7e1.chunk.js"
+  },
+  {
+    "revision": "53e5936090e752ff4a4e",
+    "url": "./static/js/runtime-main.cea9507d.js"
+  }
+]);
