1094b100810d8c60aaefd1748478af516e850b42
funfish22 <j26391442@gmail.com>
2020-01-20 12:51:40 +0800

    新增 redux初始設定

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
 import React from 'react';
+import { createStore, compose, applyMiddleware } from 'redux';
+import createSagaMiddleware from 'redux-saga';
 
-import Entrance from 'pages/App'
+import Entrance from 'pages/App';
 
+import rootReducers from 'config/library/redux/store/rootReducers';
+
+import { Provider } from 'react-redux';
+
+declare global {
+  interface Window {
+    __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose;
+  }
+}
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const sagaMiddleware = createSagaMiddleware();
+
+let store = createStore(
+  rootReducers,
+  composeEnhancers(applyMiddleware(sagaMiddleware))
+)
 
 interface Props { }
 
@@ -12,8 +32,9 @@ class App extends React.Component<Props, State> {
   render() {
 
     return (
-      <Entrance/>
-      
+      <Provider store={store}>
+        <Entrance/>
+      </Provider>
     )
   }
 }
